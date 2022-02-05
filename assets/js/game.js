433851f8a185759f632f22a6b968d3d9f5a96186
Ellyse <85855848+ellysecarter@users.noreply.github.com>
2022-02-04 18:05:45 -0800

    updated functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,11 +7,15 @@ var randomNumber = function(min, max) {
   return value;
 };
 
-// fight function (now with parameter for enemy's object holding name, health, and attack values)
-var fight = function(enemy) {
-  while (playerInfo.health > 0 && enemy.health > 0) {
-    // ask player if they'd like to fight or run
-    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+var fightOrSkip = function() {
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+  if (!promptFight){
+    window.alert('you need to provide an answer! try again')
+    fightOrSkip();
+  }
+
+  promptFight = promptFight.toLowerCase();
 
     // if player picks "skip" confirm and then stop the loop
     if (promptFight === "skip" || promptFight === "SKIP") {
@@ -23,10 +27,20 @@ var fight = function(enemy) {
         window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerInfo.money for skipping
         playerInfo.money = Math.max(0, playerInfo.money - 10);
+        
         console.log("playerInfo.money", playerInfo.money)
-        break;
+        return true;
       }
     }
+    return false;
+} 
+
+// fight function (now with parameter for enemy's object holding name, health, and attack values)
+var fight = function(enemy) {
+  while (playerInfo.health > 0 && enemy.health > 0) {
+    if (fightOrSkip()) {
+      break;
+    };
 
     // generate random damage value based on player's attack power
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
@@ -72,7 +86,7 @@ var fight = function(enemy) {
 // function to set name
   var getPlayerName = function() {
     var name = "";
-    while (name === "" || name === null){
+    while (!name){
         name = prompt("What is your robots name?")
       }; 
       window.alert("Welcome " + name + "!");
